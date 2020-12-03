9e396e2c44564f28783603508bf979d93adc7533
Demidao <demid.olenev@gmail.com>
2020-12-04 00:41:30 +0300

    1. Создан графический интерфейс на JavaFX клиента 2. Создан сам клиент. 3. Настроена сеть подключения к серверу 4. Контроллер отправляет сообщения на сервер.

---
+++ geek-chat-client/src/main/java/com/demid/geekchat/client/Network.java
@@ -0,0 +1,49 @@
+package com.demid.geekchat.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class Network {
+    private SocketChannel channel;
+    private final static String HOST = "localhost";
+    private final static int PORT = 8189;
+
+    public Network(){
+        new Thread(() -> {
+            EventLoopGroup workerGroup = new NioEventLoopGroup(); // для работы с сетью клиенту нужен пул потоков. Создаем тут этот пулf
+                                                                    // к клиенту никто не будет подключаться, поэтому пул потоков будет только один для обработки сетевых событий
+            try{
+                Bootstrap b = new Bootstrap();
+                b.group(workerGroup)
+                        .channel(NioSocketChannel.class)
+                        .handler(new ChannelInitializer<SocketChannel>() {
+
+                            @Override
+                            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                                channel = socketChannel; //когда открывается соединение, мы сохраняем его в поле класса
+                                socketChannel.pipeline().addLast(new StringDecoder(), new StringEncoder());
+                            }
+                        });
+                ChannelFuture future = b.connect(HOST, PORT).sync();
+                future.channel().closeFuture().sync();
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                workerGroup.shutdownGracefully();
+            }
+
+        }).start();
+    }
+
+    public void sendMessage(String str){ // метод отправки сообщений
+        channel.writeAndFlush(str);
+    }
+
+}
